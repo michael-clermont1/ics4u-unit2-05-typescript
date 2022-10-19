af663dc71c5590830097fdee0f1d864d81d83669
Michael Clermont <Michael.Clermont@mths.ca>
2022-10-19 09:39:43 -0400

    Update Vehicle.ts

--- src/Vehicle.ts
+++ src/Vehicle.ts
@@ -14,7 +14,7 @@ class Vehicle {
   private plate: string
   private color: string
 
-  constructor(color: string, plate: string, doors: number, maxspeed: number) {
+  constructor (color: string, plate: string, doors: number, maxspeed: number) {
     this.plate = plate
     this.color = color
     this.doors = doors
@@ -22,36 +22,36 @@ class Vehicle {
   }
 
   // getters
-  public getSpeed(): number {
+  public getSpeed (): number {
     return this.speed
   }
 
-  public getMaxSpeed(): number {
+  public getMaxSpeed (): number {
     return this.maxspeed
   }
 
-  public getDoors(): number {
+  public getDoors (): number {
     return this.doors
   }
 
-  public getPlate(): string {
+  public getPlate (): string {
     return this.plate
   }
 
-  public getColor(): string {
+  public getColor (): string {
     return this.color
   }
 
   // setters
-  public setPlate(somePlate: string): void {
+  public setPlate (somePlate: string): void {
     this.plate = somePlate
   }
 
-  public setColor(someColor: string): void {
+  public setColor (someColor: string): void {
     this.color = someColor
   }
 
-  public accelerate(accelerationPower: number, accelerationTime: number): void {
+  public accelerate (accelerationPower: number, accelerationTime: number): void {
     this.speed = accelerationPower * accelerationTime + this.speed
 
     if (this.speed > this.maxspeed) {
@@ -59,7 +59,7 @@ class Vehicle {
     }
   }
 
-  public break(breakPower: number, breakTime: number): void {
+  public break (breakPower: number, breakTime: number): void {
     this.speed = this.speed - breakPower * breakTime
 
     if (this.speed < 0) {
@@ -67,7 +67,7 @@ class Vehicle {
     }
   }
 
-  public status(): void {
+  public status (): void {
     console.log(
       `        --> Speed: ${this.speed}
         --> MaxSpeed: ${this.maxspeed}
